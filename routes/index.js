8a7490d24382d5ba1603526c1139608e7763f7a2
Robert Russo <rrusso44@gmail.com>
2015-11-07 18:11:03 -0500

    Tweaks and non-essential code removed

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-exports.index = function(req, res){
-res.render('index', { title: 'ejs' });};
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
