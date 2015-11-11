9eed902ac869a7321442074bbb55157d53743e47
Justin Etzine <justetz@gmail.com>
2015-11-11 16:48:29 -0500

    Corrected navbar active state, tweaked homepage

--- public/javascripts/controllers/MainController.js
+++ public/javascripts/controllers/MainController.js
@@ -1,3 +1,6 @@
-app.controller('MainController', ['$scope', function($scope) {
-
+app.controller('MainController', ['$scope', '$location', function($scope,$location) {
+	$scope.isCurrentPage = function(path) {
+		console.log($location.path().split('/')[1]);
+		return $location.path().split('/')[1] == path;
+	}
 }]);
