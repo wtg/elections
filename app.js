c6aa184b61ad58a2c0e22e7ed7295ca4e7cd9a7e
Justin Etzine <justetz@gmail.com>
2015-12-19 18:30:30 -0500

    Minor comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Remote Dependencies
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Routes 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
