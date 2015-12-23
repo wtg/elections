0323e2511d1631cb3b346e1d4a0347605239efca
Justin Etzine <justetz@gmail.com>
2015-12-23 18:42:31 -0500

    Got a connection working from the db to the frontend

--- routes/offices.js
+++ routes/offices.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
-var mysql = require('mysql');
-var db = require('../db.js');
+var express = require('express'),
+    router = express.Router(),
+    mysql = require('mysql'),
+    db = require('../db.js');
 
 var dbConnect = function(res) {
     var connection = mysql.createConnection(db);
@@ -31,12 +30,27 @@ router.get('/', function (req, res) {
     connection.end();
 });
 
-router.get('/:election_id', function (req, res) {
+router.get('/election/:election_id', function (req, res) {
     var connection = dbConnect(res);
 
     var election_id = req.params.election_id;
 
     connection.query("SELECT * FROM `offices` WHERE election_id = " + mysql.escape(election_id), function(err, result) {
+        if(err) {
+            console.error(err);
+            res.status(500);
+        }
+
+        res.json(result);
+    });
+
+    connection.end();
+});
+
+router.get('/types', function (req, res) {
+    var connection = dbConnect(res);
+
+    connection.query("SELECT DISTINCT type FROM `offices` WHERE NOT type = 'all'", function(err, result) {
         if(err) {
             console.error(err);
             res.status(500);
