c5e3032d8bf8ee8639b3ae2927dc9b19b6497309
Justin Etzine <justetz@gmail.com>
2015-12-24 12:09:58 -0500

    Added api endpoint for active election, added config table to hold settings

--- routes/offices.js
+++ routes/offices.js
@@ -6,13 +6,25 @@ var express = require('express'),
 var queries = {
     all: "SELECT * FROM `offices`",
     election: " WHERE election_id = ",
-    types: "SELECT DISTINCT type FROM `offices` WHERE NOT type = 'all'"
+    active_election: "(SELECT `value` FROM `configurations` WHERE `key` = 'active_election_id')",
+    types: "SELECT DISTINCT type FROM `offices` WHERE NOT type = 'all'",
+
+    post: "INSERT INTO `rpielections`.`offices` (`election_id`, `name`, `description`, `openings`, " +
+    "`nominations_required`, `type`, `disabled`) VALUES "
 };
 
 router.get('/', function (req, res) {
     var connection = functions.dbConnect(res);
 
     connection.query(queries.all, functions.defaultJSONCallback(res));
+
+    connection.end();
+});
+
+router.get('/election/active', function (req, res) {
+    var connection = functions.dbConnect(res);
+
+    connection.query(queries.all + queries.election + queries.active_election, functions.defaultJSONCallback(res));
 
     connection.end();
 });
@@ -32,6 +44,16 @@ router.get('/types', function (req, res) {
     connection.query(queries.types, functions.defaultJSONCallback(res));
 
     connection.end();
+});
+
+router.post('/', function (req, res) {
+    var connection = functions.dbConnect(res);
+
+    var data = req.body.data;
+
+    if (!data) res.status(204);
+
+    var query = queries.post + "(" + data.election_id + ", '" + "Test', 'test', '1', '125', '2020', '1')";
 });
 
 module.exports = router;
