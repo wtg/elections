1b115c71dd27d4c2bee2b36223447efec2c549b7
Justin Etzine <justetz@gmail.com>
2015-12-28 21:13:50 -0500

    Basic CRUD actions for parties, next up: party officers!

--- public/javascripts/controllers/PartiesController.js
+++ public/javascripts/controllers/PartiesController.js
@@ -1,30 +1,134 @@
-app.controller('PartiesController', ['$scope', '$http', '$cookies', '$location', function ($scope, $http, $cookies, $location) {
-    $scope.parties = [];
+app.controller('PartiesController', ['$scope', '$http', '$cookies', '$location', '$route',
+    function ($scope, $http, $cookies, $location, $route) {
+        var initialize = function () {
+            $scope.parties = [];
 
-    var initialize = function () {
-        if ($location.path().split('/')[$location.path().split('/').length-1] === 'edit' && !$scope.editPermissions) {
-            $location.url('/parties');
-        }
-    };
-    initialize();
+            if ($location.path().split('/')[$location.path().split('/').length - 1] === 'edit' && !$scope.editPermissions) {
+                $location.url('/parties');
+            }
+        };
+        initialize();
 
-    var loadData = function() {
-        $http.get('/api/parties/withleader').then(function(response) {
-            response.data.forEach(function(elem) {
-                $scope.parties.push({
-                    id: elem.party_id,
-                    name: elem.name,
-                    leader: elem.rcs_id,
-                    platform: elem.platform
+        var loadData = function () {
+            $http.get('/api/parties/withleader').then(function (response) {
+                response.data.forEach(function (elem) {
+                    $scope.parties.push({
+                        id: elem.party_id,
+                        name: elem.name,
+                        leader: elem.rcs_id,
+                        platform: elem.platform,
+                        changesPending: false
+                    });
                 });
+
+                $scope.currentEditId = $cookies.getObject("partiesEditId") ? $cookies.getObject("partiesEditId").val : $scope.parties[0].id;
+
+                $scope.new = {
+                    name: "",
+                    platform: ""
+                };
+            })
+        };
+        loadData();
+
+        $scope.setEditId = function (newId) {
+            $scope.currentEditId = newId;
+        };
+
+        /**
+         * Finds an party's index in the party array based on the ID
+         * @param id
+         * @returns {number}
+         */
+        var findParty = function (id) {
+            var position = -1;
+
+            $scope.parties.forEach(function (elem, index) {
+                if (elem.id == id) {
+                    position = index;
+                }
             });
 
-            $scope.currentEditId = $cookies.getObject("partiesEditId") ? $cookies.getObject("partiesEditId").val : $scope.parties[0].id;
-        })
-    };
-    loadData();
+            return position;
+        };
 
-    $scope.setEditId = function (newId) {
-        $scope.currentEditId = newId;
-    };
-}]);
+        /**
+         * Creates a new party
+         */
+        $scope.createParty = function () {
+            var name = $scope.new.name;
+            var preparedData = {
+                name: $scope.new.name,
+                platform: $scope.new.platform
+            };
+
+            //if (!preparedData.name) {
+            //    addNewAlert("error", "You didn't enter a title for the office!", "create");
+            //    return;
+            //} else if (!preparedData.description) {
+            //    addNewAlert("error", "You didn't enter a description for the office!", "create");
+            //    return;
+            //}
+
+            $http.post('/api/parties/create', preparedData).then(function () {
+                //addNewAlert("success", "The new office, entitled " + name + ", was created successfully!", "create");
+                alert('done');
+                $route.reload();
+            }, function (response) {
+                //addNewAlert("error", response.statusText + " (code: " + response.status + ")", "create");
+            });
+        };
+
+        /**
+         * Saves any pending edits on the currently selected office
+         */
+        $scope.saveEdits = function () {
+            var position = findParty($scope.currentEditId);
+            var name = $scope.parties[position].name;
+
+            if (isNaN($scope.currentEditId) || position == -1) {
+                return;
+            }
+
+            var preparedData = {
+                name: $scope.parties[position].name,
+                platform: $scope.parties[position].platform
+            };
+
+            //if (!preparedData.name) {
+            //    addNewAlert("error", "You didn't enter a title for the office!", "update");
+            //    return;
+            //} else if (!preparedData.description) {
+            //    addNewAlert("error", "You didn't enter a description for the office!", "update");
+            //    return;
+            //}
+
+            $http.put('/api/parties/update/' + $scope.currentEditId, preparedData).then(function () {
+                //addNewAlert("success", "The " + title + " office was successfully updated!", "update");
+                $route.reload();
+            }, function (response) {
+                //addNewAlert("error", response.statusText + " (code: " + response.status + ")", "update");
+            })
+        };
+
+        /**
+         * Deletes an office based on the given edit id
+         */
+        $scope.deleteParty = function () {
+            var position = findParty($scope.currentEditId);
+            var name = $scope.parties[position].name;
+
+            if (!confirm("Are you sure you want to permanently delete this office?") ||
+                isNaN($scope.currentEditId) || position == -1) {
+                return;
+            }
+
+            $http.delete('/api/parties/delete/' + $scope.currentEditId).then(function () {
+                //addNewAlert("success", "The " + name + " office was permanently deleted!", "delete");
+                alert("DONE WORKED");
+                $route.reload();
+            }, function (response) {
+                addNewAlert("error", response.statusText + " (code: " + response.status + ")", "delete");
+            })
+        };
+    }]);
--- routes/parties.js
+++ routes/parties.js
@@ -2,12 +2,16 @@ var express = require('express'),
     router = express.Router(),
     mysql = require('mysql'),
     functions = require('../functions.js'),
-    cms = require('../cms.js');
+    cms = require('../cms.js'),
+    logger = require('../logger.js');
 
 var queries = {
     all: "SELECT * FROM `parties`",
     withLeader: "SELECT P.*, O.rcs_id, O.position FROM `parties` P LEFT JOIN `party_officers` O ON P.party_id = " +
-    "O.party_id AND O.is_highest = 1"
+    "O.party_id AND O.is_highest = 1",
+    post: "INSERT INTO `rpielections`.`parties` (`name`, `platform`) VALUES ",
+    update: "UPDATE parties SET <> WHERE party_id = ",
+    remove: "DELETE FROM `rpielections`.`parties` WHERE party_id = "
 };
 
 router.get('/', function (req, res) {
@@ -22,6 +26,70 @@ router.get('/withleader', function (req, res) {
     var connection = functions.dbConnect(res);
 
     connection.query(queries.withLeader, functions.defaultJSONCallback(res));
+
+    connection.end();
+});
+
+router.post('/create', function (req, res) {
+    if (!functions.verifyPermissions(req).admin) {
+        res.status(401);
+    }
+
+    var connection = functions.dbConnect(res);
+
+    var data = req.body;
+
+    if (!data) res.status(204);
+
+    var query = queries.post + functions.constructSQLArray([data.name, data.platform]);
+
+    connection.query(query, functions.defaultJSONCallback(res));
+
+    logger.write(connection, req.session.cas_user, "PARTY_CREATE", "Entitled " + data.name +
+        ", platform: " + (data.platform.length === 100 ? data.platform.substr(0,100) + '...' : data.platform));
+
+    connection.end();
+});
+
+router.put('/update/:party_id', function (req, res) {
+    try {
+        if (!functions.verifyPermissions(req).admin) {
+            res.status(401);
+        }
+
+        var connection = functions.dbConnect(res),
+            party_id = req.params.party_id;
+
+        var data = req.body;
+
+        if (!data) res.status(204);
+
+        var assignments = "`name` = " + mysql.escape(data.name) + ", " +
+            "`platform` = " + mysql.escape(data.platform);
+
+        var query = queries.update.replace(/<>/g, assignments) + mysql.escape(party_id);
+
+        connection.query(query, functions.defaultJSONCallback(res));
+
+        logger.write(connection, req.session.cas_user, "PARTY_MODIFY", "Modified " + party_id + ", named " + data.name);
+
+        connection.end();
+    } catch(e) { console.log(e); }
+});
+
+router.delete('/delete/:party_id', function (req, res) {
+    if (!functions.verifyPermissions(req).admin) {
+        res.status(401);
+    }
+
+    var connection = functions.dbConnect(res),
+        party_id = req.params.party_id;
+
+    var query = queries.remove + mysql.escape(party_id);
+
+    connection.query(query, functions.defaultJSONCallback(res));
+
+    logger.write(connection, req.session.cas_user, "PARTY_DELETE", "Deleted " + party_id);
 
     connection.end();
 });
