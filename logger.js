bcfe64534eeaca3fcdc422c20fe076d6688327f1
Jason Lee <jason@mutantur.net>
2015-12-31 00:21:27 +0800

    Include current time in database logging entries

--- logger.js
+++ logger.js
@@ -10,7 +10,7 @@ exports.write = function (connection, rcsid, type, description) {
     connection = functions.dbConnect();
   }
 
-  connection.query("INSERT INTO log (rcs_id, type, description) VALUES ('" + rcsid + "', '" + type + "', '" + description + "')");
+  connection.query("INSERT INTO log (rcs_id, type, description, time) VALUES ('" + rcsid + "', '" + type + "', '" + description + "', NOW())");
 
   if(!providedConnection) {
     connection.end();
