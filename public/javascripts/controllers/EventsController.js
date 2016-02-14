06151148f78c17c46237afef730b571298026400
Justin Etzine <justetz@gmail.com>
2016-02-14 09:45:03 -0500

    Implemented read for events, closes #25

--- public/javascripts/controllers/EventsController.js
+++ public/javascripts/controllers/EventsController.js
@@ -2,23 +2,27 @@ app.controller('EventsController', ['$scope', '$http', function ($scope, $http) 
     $scope.events = [];
 
     var loadData = function() {
+        $scope.dataLoaded = false;
+        $scope.events = [];
+
         $http.get('/api/events/').then(function(response) {
-            response.data.forEach(function(elem) {
-                $scope.events.push({
-                    id: elem.event_id,
-                    title: elem.title,
-                    date: elem.date,
-                    start: elem.start,
-                    end: elem.end,
-                    location: elem.location,
-                    description: elem.description
-                });
-            });
+            $scope.events = response.data;
+            //response.data.forEach(function(elem) {
+            //    $scope.events.push({
+            //        id: elem.event_id,
+            //        title: elem.title,
+            //        date: elem.date,
+            //        start: elem.start,
+            //        end: elem.end,
+            //        location: elem.location,
+            //        description: elem.description
+            //    });
+            //});
+
             console.log($scope.events);
+        }).finally(function() {
+            $scope.dataLoaded = true;
         });
-        // Hard-setting these variables for now
-        $scope.filter = "all";
-        $scope.dataLoaded = true;
     };
     loadData();
 }]);
