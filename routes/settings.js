b0afa097bf56e2be4b4a61bd519a79390c5f57c9
Jason Lee <jason@jzblee.com>
2016-03-06 23:26:17 -0500

    Add a basic configuration API

---
+++ routes/settings.js
@@ -0,0 +1,70 @@
+var express = require('express'),
+    router = express.Router(),
+    mysql = require('mysql'),
+    functions = require('../functions.js'),
+    cms = require('../cms.js'),
+    logger = require('../logger.js');
+
+var queries = {
+    all: "SELECT * FROM " + functions.dbName() + ".`configurations`",
+    update: "UPDATE " + functions.dbName() + ".`events` SET <> WHERE key = ",
+};
+
+router.get('/', function (req, res) {
+    var connection = functions.dbConnect(res);
+
+    connection.query(queries.all, functions.defaultJSONCallback(res));
+
+    connection.end();
+});
+
+router.put('/update/:key', function (req, res) {
+    if (!functions.verifyPermissions(req).admin) {
+        res.sendStatus(401);
+        return;
+    }
+
+    var connection = functions.dbConnect(res),
+        key = req.params.key;
+
+    var data = req.body;
+
+    if (!data) {
+        res.sendStatus(204);
+        return;
+    }
+
+    var assignments = "`value` = " + mysql.escape(data.value);
+
+    var query = queries.update.replace(/<>/g, assignments) + key;
+
+    connection.query(query, functions.defaultJSONCallback(res));
+
+    logger.write(connection, req.session.cas_user, "SETTINGS_MODIFY", "Modified key " + key + ", to " + data.value);
+
+    connection.end();
+});
+
+router.delete('/delete/:event_id', function (req, res) {
+    try {
+        if (!functions.verifyPermissions(req).admin) {
+            res.sendStatus(401);
+            return;
+        }
+
+        var connection = functions.dbConnect(res),
+            event_id = req.params.event_id;
+
+        var query = queries.remove + event_id;
+
+        connection.query(query, functions.defaultJSONCallback(res));
+
+        logger.write(connection, req.session.cas_user, "EVENT_DELETE", "Deleted " + event_id);
+
+        connection.end();
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+module.exports = router;
