9aee32ee21ab15e3c0e5ff5fc9004253b6f54952
Jason Lee <jason@jzblee.com>
2016-03-21 19:30:14 -0400

    Add form validation to the elections manager

--- public/javascripts/controllers/SettingsController.js
+++ public/javascripts/controllers/SettingsController.js
@@ -1,22 +1,31 @@
 app.controller('SettingsController', ['$scope', '$http', '$cookies', '$location', '$route', '$routeParams',
     function ($scope, $http, $cookies, $location, $route, $routeParams) {
+
         var ALERTS_COOKIE_LABEL = "settingsAlerts";
+
+        var SELECT_INIT = "Select an election...";
+        var SELECT_NONE = "No elections found!";
+        var SELECT_NEW = "Creating new election...";
+        var SELECT_EDIT = "Currently editing: ";
+        var SELECT_ACTIVE = " (active)";
 
         var loadData = function () {
             $scope.dataLoaded = false;
             $scope.settings = [];
             $scope.elections = [];
+            $scope.fieldHasError = [];
             $scope.new = {
               election_name: "", primary_date: null, final_date: null, runoff_date: null
             };
-            $scope.blankElectionChoice = "Select an election...";
-            $scope.active_election_id = 0;
+            $scope.blankElectionChoice = SELECT_INIT;
+            $scope.activeElectionID = 0;
+            $scope.electionErrorText = '';
 
             $http.get('/api/settings/').then(function (response) {
                 response.data.forEach(function (elem) {
                     $scope.settings.push(elem);
                     if (elem.key === "active_election_id") {
-                      $scope.active_election_id = elem.value/1;
+                      $scope.activeElectionID = elem.value/1;
                     }
                 });
             }).then($http.get('/api/elections/').then(function (response) {
@@ -27,7 +36,7 @@ app.controller('SettingsController', ['$scope', '$http', '$cookies', '$location'
                     $scope.elections.push(elem);
                 });
                 if ($scope.elections.length === 0) {
-                  $scope.blankElectionChoice = "No elections found!";
+                  $scope.blankElectionChoice = SELECT_NONE;
                 }
             }).finally(function () {
                 $scope.dataLoaded = true;
@@ -85,28 +94,26 @@ app.controller('SettingsController', ['$scope', '$http', '$cookies', '$location'
             $cookies.putObject(ALERTS_COOKIE_LABEL, {array: $scope.alerts});
         };
 
-        $scope.numSettings = function () {
-          return $scope.settings.length;
-        };
-
         $scope.prettifyElectionName = function (elem) {
             var active = false;
             var el_string = elem.election_name;
-            if ($scope.active_election_id === elem.election_id) {
-                el_string += " (active)";
+            if ($scope.activeElectionID === elem.election_id) {
+                el_string += SELECT_ACTIVE;
             }
             if ($scope.creatingElection) {
-              el_string = "Creating new election...";
+              el_string = SELECT_NEW;
             }
             if ($scope.editingElection) {
-              el_string = "Currently editing: " + el_string;
+              el_string = SELECT_EDIT + el_string;
             }
             return el_string;
         };
 
         $scope.setElEditMode = function(mode, e) {
+          $scope.fieldHasError = [];
+          $scope.electionErrorText = "";
           if (mode === "create") {
-            $scope.blankElectionChoice = "Creating new election...";
+            $scope.blankElectionChoice = SELECT_NEW;
             $scope.creatingElection = true;
             return;
           }
@@ -119,10 +126,15 @@ app.controller('SettingsController', ['$scope', '$http', '$cookies', '$location'
             if ($scope.creatingElection) {
               var preparedData = {
                   election_name: $scope.new.election_name,
-                  primary_date: $scope.new.primary_date.toISOString().substr(0,10),
-                  final_date: $scope.new.final_date.toISOString().substr(0,10),
-                  runoff_date: $scope.new.runoff_date.toISOString().substr(0,10)
+                  primary_date: $scope.new.primary_date,
+                  final_date: $scope.new.final_date,
+                  runoff_date: $scope.new.runoff_date
               };
+
+              if (!$scope.validateForm(preparedData)) {
+                return;
+              }
+
               $http.post('/api/elections/create', preparedData).then(function () {
                   //addNewAlert("success", "The new election, entitled " + election_name + ", was created successfully!", "create");
                   $route.reload();
@@ -133,10 +145,13 @@ app.controller('SettingsController', ['$scope', '$http', '$cookies', '$location'
             else if ($scope.editingElection) {
               var preparedData = {
                   election_name: e.election_name,
-                  primary_date: e.primary_date.toISOString().substr(0,10),
-                  final_date: e.final_date.toISOString().substr(0,10),
-                  runoff_date: e.runoff_date.toISOString().substr(0,10)
+                  primary_date: e.primary_date,
+                  final_date: e.final_date,
+                  runoff_date: e.runoff_date
               };
+              if (!$scope.validateForm(preparedData)) {
+                return;
+              }
               $http.put('/api/elections/update/' + e.election_id, preparedData).then(function () {
                   //addNewAlert("success", "The election entitled " + e.election_name + " was successfully updated!", "update");
                   $route.reload();
@@ -155,9 +170,31 @@ app.controller('SettingsController', ['$scope', '$http', '$cookies', '$location'
             $scope.blankElectionChoice = "No elections found!";
           }
           else {
-            $scope.blankElectionChoice = "Select an election...";
+            $scope.blankElectionChoice = SELECT_INIT;
           }
           $scope.creatingElection = $scope.editingElection = false;
+        }
+
+        $scope.validateForm = function(data) {
+          var failedFields = 0;
+          $scope.fieldHasError = [];
+          for (var key in data) {
+            if (!data[key]) {
+              $scope.fieldHasError[key] = true;
+              failedFields++;
+
+            }
+          }
+          if (failedFields) {
+            $scope.electionErrorText = "Invalid data. Please try again.";
+            return false;
+          }
+          else {
+            data.primary_date = data.primary_date.toISOString().substr(0,10);
+            data.final_date = data.final_date.toISOString().substr(0,10);
+            data.runoff_date = data.runoff_date.toISOString().substr(0,10);
+            return true;
+          }
         }
 
         $scope.deleteElection = function(e) {
@@ -168,7 +205,7 @@ app.controller('SettingsController', ['$scope', '$http', '$cookies', '$location'
           }
 
           // Deactivate the election before deleting
-          if($scope.active_election_id === e.election_id) { $scope.setActiveEl(e); }
+          if($scope.activeElectionID === e.election_id) { $scope.setActiveEl(e); }
 
           $http.delete('/api/elections/delete/' + e.election_id).then(function () {
               //addNewAlert("success", "The election entitled " + election_name + " was permanently deleted!", "delete");
@@ -180,7 +217,7 @@ app.controller('SettingsController', ['$scope', '$http', '$cookies', '$location'
 
         $scope.setActiveEl = function (e) {
           var preparedData = { value: "" };
-          if ($scope.active_election_id === e.election_id) { preparedData.value = "0"; }
+          if ($scope.activeElectionID === e.election_id) { preparedData.value = "0"; }
           else { preparedData.value = e.election_id; }
           $http.put('/api/settings/update/active_election_id', preparedData).then(function () {
               //addNewAlert("success", "The setting, " + key + ", was updated successfully!", "create");
