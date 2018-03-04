fd105f89aef4dbe2b9b8bae8351052969d6d8aaf
Sidney Kochman <sidney@kochman.org>
2018-03-03 23:11:10 -0500

    Remove serve-favicon

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Remote Dependencies
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -58,8 +57,6 @@ var cas = new CASAuthentication({
     destroy_session: true,
 });
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, '/public', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
